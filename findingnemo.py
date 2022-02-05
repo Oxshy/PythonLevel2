e02fae2a932d2762b2d016f5cd87662f3febdb24
Akshay <randomguyon666@gmail.com>
2022-02-05 11:56:19 +0530

    adding files

---
+++ findingnemo.py
@@ -0,0 +1,42 @@
+# import math
+# print(math.factorial(8))
+# print(math.factorial.__doc__)
+# def any(x,y):
+#     """adding two numbers"""
+#     print(x+y)
+
+# any(3,6)
+# print(any.__doc__)
+
+
+
+# word="Hello"
+# index=0
+
+# for i in word:
+#     # print(word[index])
+#     print("word[",index,"] =",i)
+#     index+=1
+
+# xmas="christmas is a holiday on december 25th every year"
+# sub_str="alphabet"
+# print(xmas.find(sub_str))
+def check(x,y):
+   # print((x.find(y)))
+    if (x.find(y))==-1:
+        print( y+" is not present")
+    else:
+        print(y+" is present")
+
+Story="""A clown fish named Marlin lives in the Great Barrier Reef loses his son, Nemo. After he ventures 
+into the open sea, despite
+ his father's constant warnings about many of the ocean's dangers. Nemo is abducted by a boat and netted
+  up and sent to a dentist's office in Sydney. So, while Marlin ventures off to try to retrieve Nemo, 
+  Marlin meets a fish named Dory, a blue tang suffering from short-term memory loss. The companions 
+  travel a great distance, encountering various dangerous sea creatures such as sharks, anglerfish and
+   jellyfish, in order to rescue Nemo from the dentist's office, which is situated by Sydney Harbor. 
+   While the two are doing this, Nemo and the other sea animals in the dentist's fish tank plot a way 
+   to return to Sydney Harbor to live their lives free again."""
+
+finder=input("Which word do you want to search?")
+check(Story,finder)
